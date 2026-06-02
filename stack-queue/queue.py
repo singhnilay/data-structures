b2d33fd1bd53f78ba5f5d8a2464fdfaaf4bdd5a8
itsnilayyy@gmail.com <ayenilay@Nilays-MacBook-Air.local>
2026-06-02 17:20:44 +0530

    Added Queue and Stack methods

---
+++ stack-queue/queue.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length += 1
+
+    def print_queue(self):
+        temp = self.first
+
+        while temp:
+            print(temp.value)
+            temp = temp.next 
+
+    def enqueue(self, value):
+
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+            
+        else:
+            self.last.next = new_node
+            self.last = new_node
+
+        self.length += 1
+
+    def dequeue(self):
+
+        if self.length == 0:
+            return None
+
+        if self.length == 1:
+            oldFirst = self.first
+            self.first = None
+            self.last = None
+            self.length -= 1
+            return oldFirst
+
+        else:
+            temp = self.first
+            self.first = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
